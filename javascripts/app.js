6dda6285e43d68c088b0268f8c0bea3eafe9a3d8
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-08-06 00:08:25 +0200

    iteration 3

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,6 +3,8 @@ const directions = ["N","E","S","W"]
 // ======================
 var rover = {
   direction : 0,
+  x : 0,
+  y : 0,
   turnLeft : function () {
     prevDirection=this.direction
     if(this.direction==0){
@@ -18,21 +20,44 @@ var rover = {
       this.direction=0
     }
     console.log("Turn from "+directions[prevDirection]+" to "+directions[this.direction]+" direction")
+  },
+  moveForward : function () {
+    prevX=this.x
+    prevY=this.y
+    switch (directions[this.direction]) {
+      case "N":
+        this.y--
+        break;
+      case "E":
+        this.x++
+        break;
+      case "S":
+        this.y++
+        break;
+      case "W":
+        this.x--
+        break;
+      default:
+        console.log("invalid direction")
+        break;
+    }
+    console.log("Move from ["+prevX+","+prevY+"] to ["+this.x+","+this.y+"] position")
   }
 }
 // ======================
 function turnLeft(rover){
-  console.log("turnLeft was called!");
+  //console.log("turnLeft was called!");
   rover.turnLeft()
 }
 
 function turnRight(rover){
-  console.log("turnRight was called!");
+  //console.log("turnRight was called!");
   rover.turnRight()
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  //console.log("moveForward was called")
+  rover.moveForward(rover)
 }
 
 //test
@@ -44,3 +69,7 @@ turnRight(rover)
 turnRight(rover)
 turnRight(rover)
 turnRight(rover)
+turnRight(rover)
+moveForward(rover)
+turnRight(rover)
+moveForward(rover)
